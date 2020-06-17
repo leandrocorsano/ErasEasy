94bbef01fe7bd671d19deadd73987ec14356dfeb
Francesca-Rossi <66570631+Francesca-Rossi@users.noreply.github.com>
2020-06-17 23:16:18 +0200

    File della parte server del db
    File base, devo ancora implementarlo

---
+++ DBManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DB_Manager
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "DBManager" nel codice e nel file di configurazione contemporaneamente.
+    public class DBManager : IDBManager
+    {
+        public SqlConnection DBconnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DBdelate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DBinsert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlDataReader DBselect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DBupdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace DB_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost DBmanager = new ServiceHost(typeof(DBManager));
+                DBmanager.Open();
+                Console.WriteLine("Server database attivo, premi un tasto per interrompere");
+                Console.ReadLine();
+                DBmanager.Close();
+                Console.WriteLine("Server database chiudo");
+                }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Errore" + ex.ToString());
+            }
+        }
+    }
+}
