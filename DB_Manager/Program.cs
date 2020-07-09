c43da5ad264c45e0d0b06bc61a46d71a9f5474bc
Francesca Rossi <francesca.rossi15@studenti.unipr.it>
2020-07-10 00:11:59 +0200

    Aggiornamento WCF Db Manager ora  la connesisone funziona

--- DB_Manager/Program.cs
+++ DB_Manager/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace DB_Manager
 {
@@ -19,8 +20,42 @@ namespace DB_Manager
                 ServiceHost DBmanager = new ServiceHost(typeof(DBManager));
                 DBmanager.Open();
                 Console.WriteLine("Server database attivo, premi un tasto per interrompere");
-                //bool inserisci = prova.DBinsert("Prova", "(2,'marco', 'rossi')");
-                SqlDataReader p = prova.DBselect("nome", "Prova");
+                //prova.DBconnection();
+                /*try
+                {
+                    string s1= "INSERT INTO `Studente`(`IdStud`, `Nome`, `Cognome`, `Data_n`) VALUES (3, 'Giacomo','Neri','2002-04-10')";
+                    string s2 = "INSERT INTO `Studente`(`IdStud`, `Nome`, `Cognome`, `Data_n`) VALUES (4, 'Giorgia','Bianchi','1992-04-10')";
+                    List<string> query= new List<string>();
+                    query.Add(s1);
+                    query.Add(s2);//
+                    var transazione = prova.DBtransaction(query);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }*/
+                /*try
+                {
+                    //bool inserisci = prova.DBinsert("Studente", "(2, 'Marco','Rossi','2000-03-20')", "(`IdStud`, `Nome`, `Cognome`, `Data_n`)");
+                    MySqlDataReader selezione = prova.DBselect("*", "Studente", "Cognome='Rossi'");
+                    while (selezione.Read())
+                    {
+                        Console.WriteLine(string.Format(
+                            "Reading from table=({0}, {1}, {2})",
+                            selezione.GetInt32(0),
+                            selezione.GetString(1),
+                            selezione.GetString(2),
+                            selezione.GetString(3)));
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }*/
+                
+
+                //SqlDataReader p = prova.DBselect("nome", "Prova");
                 Console.ReadLine();
                 DBmanager.Close();
                 Console.WriteLine("Server database chiuso");
