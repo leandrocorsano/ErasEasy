7c1a00cdfcdce60c69b8fafe226d524f22baee44
Francesca Rossi <francesca.rossi15@studenti.unipr.it>
2020-08-03 06:30:40 +0200

    Creazione wcf admin e collegamento con DBManager

---
+++ Admin_Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Admin_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
