1b20ac2fb4629cba8c4675f70b4f2b3e69ce1c32
Francesca Rossi <francesca.rossi15@studenti.unipr.it>
2020-08-10 06:43:00 +0200

    Aggiunta del template ci sono dei miglioramenti da fare

--- ErasEasyLife/App_Start/BundleConfig.cs
+++ ErasEasyLife/App_Start/BundleConfig.cs
@@ -19,12 +19,21 @@ namespace ErasEasyLife
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            // bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            // "~/Scripts/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                        "~/Scripts/bootstrap.bundle.min.js"));
 
+            /* bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/bootstrap.css",
+                       "~/Content/site.css"));*/
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/adminlte/css/adminlte.min.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/adminlte/js").Include(
+             "~/adminlte/js/adminlte.min.js"));
         }
     }
 }
--- ErasEasyLife/Models/Associazione.cs
+++ ErasEasyLife/Models/Associazione.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Runtime.Serialization;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ErasEasyLife.Models
 {
@@ -13,18 +16,32 @@ namespace ErasEasyLife.Models
         [DataMember]
         public int IdAss { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "Il nome dell'associazione è obbligatorio")]
         public string nome { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "La città dell'associazione è obbligatoria")]
+        [Display(Name = "Città")]
         public string citta { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "Lo stato dell'associazione è obbligatorio")]
         public string stato { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "La via della sede è obbligatoria")]
         public string via { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "Il cellulare è obbligatorio")]
+        [Display(Name = "Telefono")]
         public string tel { get; set; }
         [DataMember]
+        [Required(ErrorMessage = "L'email è obbligatoria")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Perfavore inserisci un email valida") ]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Perfavore inserisci una mail valida")]
         public string email { get; set; }
+
         [DataMember]
+        [Required(ErrorMessage = "Password obbligatoria")]
+        [MinLength(6, ErrorMessage = "La password de contenere almeno 6 caratteri alfanumerici")]
+        [RegularExpression(@"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}", ErrorMessage = "La password deve contenere almeno 1 lettera maiuscola, 1 minuscola e 1 numero e deve essere di almeno 6 caratteri")]
         public string password { get; set; }
     }
 }
