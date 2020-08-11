2b5bb1f36b8a3baec167f41cd0113233857fbd50
Francesca Rossi <francesca.rossi15@studenti.unipr.it>
2020-08-11 08:16:45 +0200

    Inizio creazione home page e modifica template

--- ErasEasyLife/App_Start/BundleConfig.cs
+++ ErasEasyLife/App_Start/BundleConfig.cs
@@ -29,11 +29,21 @@ namespace ErasEasyLife
                        "~/Content/site.css"));*/
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/adminlte/plugins/fontawesome-free/css/all.min.css",
                       "~/adminlte/css/adminlte.min.css",
+                      "~/adminlte/plugins/fullcalendar/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-daygrid/main.min.css",
+                      "~/adminlte/plugins/fullcalendar-bootstrap/main.min.css",
                       "~/Content/site.css"));
 
             bundles.Add(new ScriptBundle("~/adminlte/js").Include(
-             "~/adminlte/js/adminlte.min.js"));
+             "~/adminlte/js/adminlte.min.js",
+             "~/adminlte/plugins/fullcalendar/main.js",
+             "~/adminlte/plugins/fullcalendar-daygrid/main.js",
+             "~/adminlte/plugins/fullcalendar-bootstrap/main.js"
+             ));
+           
+          
         }
     }
 }
