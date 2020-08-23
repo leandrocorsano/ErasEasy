9b84593630a77455378101ef063690bcd74dfabc
Leandro Corsano <leandro.corsano97@gmail.com>
2020-08-23 15:52:12 +0200

    probl elenco volontario

--- ErasEasyLife/Controllers/VolontarioController.cs
+++ ErasEasyLife/Controllers/VolontarioController.cs
@@ -254,6 +254,29 @@ namespace ErasEasyLife.Controllers
             }
 
         }
+        [HttpGet]
+        public ActionResult Elenco()
+        {
+            return View();
+        }
+        [HttpPost]
+        // GET: Volontario/Create
+        public ActionResult Elenco(Models.Volontario model)
+        {
+            if (ModelState.IsValidField("ass"))
+            {
+                var webclient = new Volunteer.VolunteerClient();
+                Volunteer.Volontario vol = (Volunteer.Volontario)Session["Volontario"];
+                string cond = "idvolont!" + vol.IdVolont + " and ass='" + model.ass + "'";
+                Volunteer.Associazione[] listaass = webclient.GetAssociazione();
+                ViewData["Associazione"] = listaass;
+                Volunteer.Volontario[] volontari = webclient.Show_volontari(cond);
+
+                ViewData["volontari"] = volontari;
+                return View();
+            }
+            return View();
+        }
 
         // GET: Volontario/Delete/5
         public ActionResult Delete(int id)
