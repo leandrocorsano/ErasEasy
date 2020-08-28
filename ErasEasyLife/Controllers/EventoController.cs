40d909039dbced0d94c58183995d5168b5b2e71a
Francesca Rossi <francesca.rossi15@studenti.unipr.it>
2020-08-28 18:12:56 +0200

    Creazione della pagina lista eventi

---
+++ ErasEasyLife/Controllers/EventoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErasEasyLife.Controllers
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+        [HttpGet]
+        public ActionResult Lista_Eventi()
+        {
+            DateTime oggi = DateTime.Today;
+            string cond = " and data_i > '" + oggi.ToString("yyyy-MM-dd") + "' and tipologia!='riunione' order by data_i";
+            var webclient = new Event.EventClient();
+            Event.Svolgimento[] eventi = webclient.Show_events(cond);
+
+            ViewData["eventi"] = eventi;
+            return View();
+        }
+    }
+}
